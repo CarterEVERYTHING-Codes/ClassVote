ad190e3c46e4826c6af7d5b271058ae7f586cd86
Carter Matson <cartermat9876@gmail.com>
2025-06-12 14:15:51 +0000

    Im about to do our first release. Make sure everything we dont need is g

--- src/app/my-results/page.tsx
+++
@@ -1,8 +0,0 @@
-// This file is no longer needed as its functionality has been merged into /results/page.tsx
-// You can safely delete this file from your project.
-// If you prefer to keep it as a placeholder, you can leave this comment.
-export default function MyResultsPage_DEPRECATED() {
-  return null;
-}
-
-    
--- src/components/admin-panel.tsx
+++
@@ -1,3 +0,0 @@
-// This file is no longer used and can be safely deleted.
-// Admin controls have been integrated directly into src/app/session/[sessionId]/page.tsx.
-export {};
